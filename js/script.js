c064afa59774cb6d9dc13e1358d4b65fd36d9d00
João Victor <108486349+k4im@users.noreply.github.com>
2023-03-03 10:40:11 -0300

    jsFeatures

--- js/script.js
+++ js/script.js
@@ -6,6 +6,18 @@ const sectionProcess = document.querySelector(".section__processo");
 const containerForm = document.getElementById("container__form");
 const sectionSobre = document.querySelector(".sobre__text-box");
 const footer = document.querySelector(".footer__container");
+const hiddenBtn = document.querySelector(".home-link");
+const hamburgerMenu = document.querySelector(".hamburguer-menu");
+
+const homeBtn = document.getElementById("home--btn");
+const contatoBtn = document.getElementById("contato--btn");
+const sobreBtn = document.getElementById("sobre--btn");
+
+const home = document.getElementById("section--home");
+const projetos = document.getElementById("section--projetos");
+const contato = document.getElementById("section--contact");
+const sobre = document.getElementById("section--sobre");
+
 
 /*Adição de classe nas variaveis*/
 boxes.classList.add("load--item");
@@ -14,13 +26,18 @@ sectionProcess.classList.add("load--item");
 containerForm.classList.add("load--item");
 sectionSobre.classList.add("load--item");
 footer.classList.add("load--item");
-
+hiddenBtn.classList.add("load--item");
 /*Definição da função de remoção de classe*/
 function visibleClass (entries, observer) {
     const [entry] = entries;
     if(entry.isIntersecting) entry.target.classList.remove("load--item");
 
 };
+function btnShow(entries) {
+    const [entry] = entries
+    if(entry.isIntersecting) hiddenBtn.classList.remove("load--item");
+    else if (!entry.isIntersecting) hiddenBtn.classList.add("load--item");
+}
 /*Padrão de 25% de visibilidade para o observador ativar*/
 const defaultObsOptions = {
     root: null,
@@ -39,6 +56,10 @@ const sectionObs = new IntersectionObserver(visibleClass, fiftyObsOpt);
 const containerObs = new IntersectionObserver(visibleClass, defaultObsOptions)
 const sobreObs = new IntersectionObserver(visibleClass, fiftyObsOpt);
 const footerObs = new IntersectionObserver(visibleClass, defaultObsOptions);
+const obsBtn = new IntersectionObserver(btnShow, {
+    root: null,
+    threshold: .1
+})
 
 /*Oberservando*/
 obsBoxes.observe(boxes);
@@ -47,7 +68,31 @@ sectionObs.observe(sectionProcess);
 containerObs.observe(containerForm);
 sobreObs.observe(sectionSobre);
 footerObs.observe(footer);
-
+obsBtn.observe(footer);
 
 // ================= final de observadores
+function scrollToSection(element) {    
+    window.scrollTo({
+        behavior: "smooth",
+        top: element.offsetTop
+    })
+};
+homeBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    scrollToSection(home);
+});
 
+contatoBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    scrollToSection(contato);
+});
+
+sobreBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    scrollToSection(sobre);
+});
+
+hiddenBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    scrollToSection(home);
+})
